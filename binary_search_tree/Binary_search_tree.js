4c0569edde8afd64076a780a0c640bb7de09ebd8
lilyhoratio <lilyhzhou@gmail.com>
2019-12-18 17:26:11 -0500

    fixed insert

--- binary_search_tree/Binary_search_tree.js
+++ binary_search_tree/Binary_search_tree.js
@@ -5,18 +5,18 @@ class BinarySearchTree {
     this.right = null;
   }
   insert(value) {
-    // BASE CASE
-    //   if node.value is null, there is no root!
+    // if there is no root
     if (!this.value) {
       this.value = value;
     } else if (value > this.value) {
-      // go right. if right is empty, insert value. else recur.
+      // go right. if right is empty, insert value (base case). else recur.
       if (!this.right) {
         this.right = BinarySearchTree(value);
       } else {
+        this.right.insert(value);
       }
     } else if (value < this.value) {
-      // go left. if left is empty, insert value. else recur
+      // go left. if left is empty, insert value (base case). else recur
       if (!this.left) {
         this.left = BinarySearchTree(value);
       } else {
